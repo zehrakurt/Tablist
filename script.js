348b500f80c43e65cc4a4ce4e98c01c23a887865
zehrakurt <153917389+zehrakurt@users.noreply.github.com>
2024-03-15 13:08:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 document.getElementById("tab1-button").addEventListener("click", function() {
-    // Tüm tab-content'leri gizle
+    
     let tabContents = document.getElementsByClassName("tab-content");
     for (var i = 0; i < tabContents.length; i++) {
         tabContents[i].style.display = "none";
     }
-    // Sadece tıklanan tab-content'i göster
+  
     document.getElementById("tab1-content").style.display = "block";
 });
 
 document.getElementById("tab2-button").addEventListener("click", function() {
-    // Tüm tab-content'leri gizle
+    
     let tabContents = document.getElementsByClassName("tab-content");
     for (var i = 0; i < tabContents.length; i++) {
         tabContents[i].style.display = "none";
     }
-    // Sadece tıklanan tab-content'i göster
+    
     document.getElementById("tab2-content").style.display = "block";
 });
 
 document.getElementById("tab3-button").addEventListener("click", function() {
-    // Tüm tab-content'leri gizle
+   
     let tabContents = document.getElementsByClassName("tab-content");
     for (var i = 0; i < tabContents.length; i++) {
         tabContents[i].style.display = "none";
     }
-    // Sadece tıklanan tab-content'i göster
+   
     document.getElementById("tab3-content").style.display = "block";
 });
 
